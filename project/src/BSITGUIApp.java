bb57f06ec960bf10c3d4f928db6f5bc317d8154e
edwin718 <159702304+edwin718@users.noreply.github.com>
2024-03-06 18:25:11 +0300

    SWING  APPLICATION
    This is the header of a code in java that is used to create a graphical user interface known as swing application.

---
+++ project/src/BSITGUIApp.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class BSITGUIApp extends JFrame {
+    public static void main(String[] args) {
+        new BSITGUIApp();
+    }
+    public BSITGUIApp(){
+        setTitle("swingApplication");
+        setSize(250,150);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
